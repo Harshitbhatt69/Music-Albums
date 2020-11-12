2c7042571dcc15a647d2fd6d65dc4b49dcfdc2e7
Harshit Bhatt <harshitbhatt972@gmail.com>
2020-11-12 11:37:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//import library to create component
+import React from 'react';
+import {AppRegistry, View} from 'react-native';
+import AlbumList from './src/components/AlbumList';
+import Header from './src/components/header';
+
+//create component
+// eslint-disable-next-line no-undef
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+//render it to the device
+AppRegistry.registerComponent('albums', () => App);
